aa6f8aa8d86aa1babe46369cf4771cbf54a1ab07
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-05-31 15:08:59 -0400

    is it still cross-platform if you write everything twice but hide it behind #cfg?

--- playground/src/lib.rs
+++ playground/src/lib.rs
@@ -1,27 +1,30 @@
 use wasm_bindgen::prelude::*;
 
+#[cfg(target_arch = "wasm32")]
+fn build<T>(builder: rayon::ThreadPoolBuilder<T>) -> rayon::ThreadPool {
+    let pool = wasm_executor::WorkerPool::new(5).expect("pool creation failed");
+    builder
+        .spawn_handler(move |thread| Ok(pool.run(|| thread.run()).unwrap()))
+        .build()
+        .unwrap()
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn build(builder: rayon::ThreadPoolBuilder) -> rayon::ThreadPool {
+    builder.build().unwrap()
+}
+
 #[wasm_bindgen]
 pub fn main() {
+    #[cfg(target_arch = "wasm32")]
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    #[cfg(target_arch = "wasm32")]
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     let concurrency = 5;
-
-    let pool = wasm_executor::WorkerPool::new(concurrency).expect("pool creation failed");
-    pool.run(|| {
-        log::info!("threadzz!");
-    })
-    .expect("run failed");
-
-    let thread_pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(concurrency)
-        .spawn_handler(|thread| Ok(pool.run(|| thread.run()).unwrap()))
-        .build()
-        .unwrap();
-    pool.run(move || {
-        thread_pool.install(|| {
-            log::info!("what's good?");
-        });
-    })
-    .expect("run failed");
+    let thread_pool = build(rayon::ThreadPoolBuilder::new().num_threads(concurrency));
+    thread_pool.spawn(|| {
+        log::info!("what's good?");
+    });
+    log::info!("ayy");
 }
